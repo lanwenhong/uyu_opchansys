312b0e494e01a675ef552e722558a60ae4d51ef3
richard-deng <richard.deng@live.com>
2017-03-16 11:23:08 +0800

    修改

--- bin/handler/store.py
+++ bin/handler/store.py
@@ -105,7 +105,7 @@ class StoreInfoHandler(core.Handler):
             return []
 
         for item in data:
-            user_ret = self.db.select_ont(table='auth_user', fields='phone_num', where={'id': item['userid']})
+            user_ret = self.db.select_one(table='auth_user', fields='phone_num', where={'id': item['userid']})
             item['phone_num'] = user_ret.get('phone_num') if user_ret else ''
             item['create_time'] = item['ctime'].strftime('%Y-%m-%d %H:%M:%S')
             item['store_type'] = UYU_STORE_ROLE_MAP.get(item['store_type'], '')
