a4c78e4ba44be4ee48d2aa8633a567c0faab348e
lanwenhong <hexiexuanlv@126.com>
2017-03-24 15:26:20 +0800

    fixbug

--- conf/config.py
+++ conf/config.py
@@ -11,7 +11,7 @@ LOGFILE = {
         }
     }
 }
-# LOGFILE = None
+LOGFILE = None
 
 # 数据库配置
 database = {
@@ -51,8 +51,8 @@ template = os.path.join(HOME, 'template')
 # 服务地址
 HOST = '0.0.0.0'
 # 服务端口
-PORT = 8084
+PORT = 8083
 #redis
 redis_url = 'redis://127.0.0.1:4600/0'
 #cookie 配置
-cookie_conf = { 'expires':60*60*24*3, 'max_age':60*60*24*3, 'domain':'121.40.177.111', 'path':'/'}
+cookie_conf = { 'expires':60*60*24*3, 'max_age':60*60*24*3, 'domain':'121.40.177.111', 'path':'/channel_op'}
