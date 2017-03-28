8e636b7d046b04a15de9f0c82f063da2d7ab8380
richard-deng <richard.deng@live.com>
2017-03-28 22:40:00 +0800

    添加

--- bin/handler/settle.py
+++ bin/handler/settle.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+import traceback
 from zbase.web import core
 from zbase.web import template
+from uyubase.uyu import define
 from uyubase.uyu.define import UYU_USER_ROLE_SUPER, UYU_USER_STATE_OK, UYU_SYS_ROLE_OP
 from uyubase.base.usession import uyu_check_session, uyu_check_session_for_page
+from zbase.web.validator import with_validator_self, Field, T_REG, T_INT, T_STR, T_FLOAT
+from zbase.base.dbpool import with_database
+from uyubase.base.uyu_user import UUser
+
 from runtime import g_rt
 from config import cookie_conf
 import logging, datetime, time
@@ -15,3 +21,65 @@ class SettleManage(core.Handler):
     def GET(self):
         self.write(template.render('settle.html'))
 
+
+class SettleInfoHandler(core.Handler):
+
+    _get_handler_fields = [
+        Field('page', T_INT, False),
+        Field('maxnum', T_INT, False),
+    ]
+
+    def _get_handler_errfunc(self, msg):
+        return error(UAURET.PARAMERR, respmsg=msg)
+
+    @uyu_check_session(g_rt.redis_pool, cookie_conf, UYU_SYS_ROLE_OP)
+    @with_validator_self
+    def _get_handler(self, *args):
+        if not self.user.sauth:
+            return error(UAURET.SESSIONERR)
+        try:
+            data = {}
+            params = self.validator.data
+            curr_page = params.get('page')
+            max_page_num = params.get('maxnum')
+
+            start, end = tools.gen_ret_range(curr_page, max_page_num)
+            info_data = self._query_handler()
+
+            data['info'] = self._trans_record(info_data[start:end])
+            data['num'] = len(info_data)
+        except Exception as e:
+            log.warn(traceback.format_exc())
+            return error(UAURET.DATAERR)
+
+    @with_database('uyu_core')
+    def _query_handler(self):
+        where = {}
+        keep_fields = '*'
+        ret = self.db.select(table='settlement_record', fields=keep_fields, where=where)
+        return ret
+
+    @with_database('uyu_core')
+    def _trans_record(self, data):
+        if not data:
+            return []
+
+        for item in data:
+            channel_ret = tools.channel_id_to_name(item['channel_id'])
+            store_ret = tools.store_id_to_name(item['store_id'])
+            item['channel_name'] = channel_ret.get('channel_name') if channel_ret else ''
+            item['store_name'] = store_ret.get('store_name') if store_ret else ''
+            item['settle_cycle'] = item['settle_cycle'].strftime('%Y-%M-%d')
+            item['settle_time'] = item['settle_time'].strftime('%Y-%M-%d')
+
+        return data
+
+
+    def GET(self):
+        try:
+            data = self._get_handler()
+            return data
+        except Exception as e:
+            log.warn(e)
+            log.warn(traceback.format_exc())
+            return error(UAURET.SERVERERR)
