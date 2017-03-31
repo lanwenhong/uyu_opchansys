08ce1dff07bef6561709f128d4c40b6af743ceff
richard-deng <richard.deng@live.com>
2017-03-31 11:06:07 +0800

    修改

--- bin/urls.py
+++ bin/urls.py
@@ -9,6 +9,7 @@ from handler import device
 from handler import train
 from handler import settle
 from handler import eyesight
+from handler import rules
 urls = (
     ('/ping', ping.Ping),
     #页面
@@ -64,4 +65,7 @@ urls = (
 
     #结算
     ('^/channel_op/v1/api/settle_list$', settle.SettleInfoHandler),
+
+    #套餐规则
+    ('^/channel_op/v1/api/rules_list$', rules.RulesInfoHandler),
 )
