9cca33bef877b246eccc72731de11dae8f185c36
lanwenhong <hexiexuanlv@126.com>
2017-04-08 11:00:47 +0800

    fixbug

--- conf/config.py
+++ conf/config.py
@@ -14,12 +14,25 @@ LOGFILE = {
 # LOGFILE = None
 
 # 数据库配置
-database = {
-    'uyu_core': {
-        'token': 'server_core_uyu',
-        'conn': 5,
-        'engine': 'pymysql'
-    },
+#database = {
+#    'uyu_core': {
+#        'token': 'server_core_uyu',
+#        'conn': 5,
+#        'engine': 'pymysql'
+#    },
+#}
+
+database = {'uyu_core': 
+    {   
+        'engine': 'pymysql', 
+        'passwd': '123456', 
+        'charset': 'utf8', 
+        'db': 'ychannel', 
+        'idle_timeout': 60, 
+        'host': '127.0.0.1', 
+        'user': 'yyy', 
+        'port': 3306, 
+        'conn': 5}
 }
 
 # web config
@@ -51,7 +64,7 @@ template = os.path.join(HOME, 'template')
 # 服务地址
 HOST = '0.0.0.0'
 # 服务端口
-PORT = 8084
+PORT = 8184
 #redis
 redis_url = 'redis://127.0.0.1:4600/0'
 #cookie 配置
